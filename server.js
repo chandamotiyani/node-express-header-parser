9b576bc9c9d3fb860e5d1b62017699336b35ab35
Chanda <motiyanichanda.1992@gmail.com>
2020-03-28 15:37:04 +1030

    completed code for API Project: Request Header Parser Microservice

---
+++ server.js
@@ -0,0 +1,39 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+//code for API Project: Request Header Parser Microservice
+app.get("/api/whoami", function(req,res) {
+  var ip_strings = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  var ip = ip_strings.split(",");
+  var user_lnaguage = req.headers['accept-language'];
+  var software = req.headers['user-agent'];
+  res.json({"ipaddress": ip[0], "language" : user_lnaguage, "software" : software});
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
